54c85c6bf7acd6d786f381182336ecbc507801cf
ayush sable <ayushsable@ayushs-MacBook-Air.local>
2025-07-16 11:37:47 +0530

    Post_Api

--- backend/routes/WorkoutRoutes.js
+++ backend/routes/WorkoutRoutes.js
@@ -1,10 +1,12 @@
-
 const express = require('express');
 const router = express.Router();
 
-const { getAllWorkouts } = require('../controllers/WorkoutController');
+const { getAllWorkouts, createWorkout } = require('../controllers/WorkoutController');
 
-
+// GET all workouts
 router.get('/', getAllWorkouts);
+
+// POST a new workout
+router.post('/', createWorkout);
 
 module.exports = router;
